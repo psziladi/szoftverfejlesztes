f3b4193d212d748a496860bde2d3224b4d7e97e5
psziladi <psziladi@gmail.com>
2024-03-14 13:04:18 +0100

    Ver1

---
+++ HajosKvíz/HajosKvíz/Form1.cs
@@ -0,0 +1,90 @@
+namespace HajosKvíz
+{
+    public partial class Form1 : Form
+    {
+        List<Kérdés> ÖsszesKérdés;
+        List<Kérdés> AktuálisKérdések;
+
+        VálaszGomb VálaszGomb1;
+        VálaszGomb VálaszGomb2;
+        VálaszGomb VálaszGomb3;
+
+        int AktuálisKérdés = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            VálaszGomb1 = new VálaszGomb();
+            VálaszGomb1.Top = 40;
+            Controls.Add(VálaszGomb1);
+
+            VálaszGomb2 = new VálaszGomb();
+            VálaszGomb2.Top = 100;
+            Controls.Add(VálaszGomb2);
+
+            VálaszGomb3 = new VálaszGomb();
+            VálaszGomb3.Top = 160;
+            Controls.Add(VálaszGomb3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ÖsszesKérdés = KérdésBetöltés();
+            AktuálisKérdések = new List<Kérdés>();
+            for (int i = 0; i < 7; i++)
+            {
+                AktuálisKérdések.Add(ÖsszesKérdés[0]);
+                ÖsszesKérdés.RemoveAt(0);
+            }
+
+            dataGridView1.DataSource = AktuálisKérdések;
+            Kérdésmegjelenítés(AktuálisKérdések[AktuálisKérdés]);
+        }
+
+        void Kérdésmegjelenítés(Kérdés kérdés)
+        {
+            label1.Text = kérdés.KérdésSzöveg;
+            VálaszGomb1.Text = kérdés.Válasz1;
+            VálaszGomb2.Text = kérdés.Válasz2;
+            VálaszGomb3.Text = kérdés.Válasz3;
+
+            if (string.IsNullOrEmpty(kérdés.URL))
+            {
+                pictureBox1.Visible = false;
+
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Load("https://storage.altinum.hu/hajo/" + kérdés.URL);
+            }
+        }
+
+
+        List<Kérdés> KérdésBetöltés()
+        {
+            List<Kérdés> kérdések = new List<Kérdés>();
+            StreamReader sr = new StreamReader("hajozasi_szabalyzat_kerdessor_BOM.txt", true);
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine() ?? "n/a";
+                string[] tömb = sor.Split("\t");
+
+                if (tömb.Length != 7) continue;
+                Kérdés k = new Kérdés();
+                k.KérdésSzöveg = tömb[1];
+                k.Válasz1 = tömb[2];
+                k.Válasz2 = tömb[3];
+                k.Válasz3 = tömb[4];
+                k.URL = tömb[5];
+                int x = 0;
+                int.TryParse(tömb[6], out x);
+                k.HelyesVálasz = x;
+                kérdések.Add(k);
+            }
+            sr.Close();
+
+            return kérdések;
+        }
+    }
+}
---
+++ HajosKvíz/HajosKvíz/VálaszGomb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajosKvíz
+{
+    internal class VálaszGomb : TextBox
+    {
+        public VálaszGomb()
+        {
+            BackColor = Color.LightGray;
+            Multiline = true;
+            ReadOnly = true;
+            BorderStyle = BorderStyle.None;
+            Width = 300;
+            Height = 50;
+            MouseEnter += VálaszGomb_MouseEnter;
+            MouseLeave += VálaszGomb_MouseLeave;
+
+            BorderStyle = BorderStyle.None;
+            Cursor = Cursors.Hand;
+        }
+
+        private void VálaszGomb_MouseLeave(object? sender, EventArgs e)
+        {
+            BorderStyle = BorderStyle.None;
+        }
+
+        private void VálaszGomb_MouseEnter(object? sender, EventArgs e)
+        {
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+    }
+}
